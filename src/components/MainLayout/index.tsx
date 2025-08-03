712cee7116022712894224af4b0f8403e5e2f835
saltedfish6 <2386298948@qq.com>
2025-08-03 22:06:02 +0800

    feat(components): 优化导航栏设计

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -5,15 +5,16 @@ import {
   useLocation // 路由信息
 } from 'react-router-dom'
 import { Tabbar } from 'react-vant'
-import { HomeO, Search, FriendsO, SettingO, UserO } from '@react-vant/icons'
+import { HomeO, LocationO, TodoListO, ChatO, UserO } from '@react-vant/icons'
+import styles from './main-layout.module.styl'
 
-// 菜单配置
+// 菜单配置 - 更新图标和标题
 const tabs = [
   { icon: <HomeO />, title: '首页', path: '/home' },
-  { icon: <Search />, title: '地图', path: '/map' },
-  { icon: <FriendsO />, title: '行程', path: '/itinerary' },
-  { icon: <SettingO />, title: 'AI助手', path: '/aiassistant' },
-  { icon: <UserO />, title: '我的', path: '/user' }
+  { icon: <LocationO />, title: '地图', path: '/map' },
+  { icon: <TodoListO />, title: '行程', path: '/itinerary' },
+  { icon: <ChatO />, title: 'AI助手', path: '/aiassistant' },
+  { icon: <UserO />, title: '个人中心', path: '/user' }
 ]
 
 const MainLayout = () => {
@@ -33,26 +34,28 @@ const MainLayout = () => {
   }, [location.pathname])
 
   return (
-    <>
+    <div style={{ paddingBottom: '100px' }}>
       <Outlet />
-      {/* tabbar */}
-      <Tabbar
-        value={active}
-        onChange={(key) => {
-          const index = Number(key)
-          setActive(index)
-          navigate(tabs[index].path)
-        }}
-      >
-        {tabs.map((tab, index) => {
-          return (
-            <Tabbar.Item key={index} icon={tab.icon}>
-              {tab.title}
-            </Tabbar.Item>
-          )
-        })}
-      </Tabbar>
-    </>
+      {/* 自定义样式的底部导航栏 - 悬浮在底部并留有空间 */}
+      <div className={styles.customTabbar}>
+        <Tabbar
+          value={active}
+          onChange={(key) => {
+            const index = Number(key)
+            setActive(index)
+            navigate(tabs[index].path)
+          }}
+        >
+          {tabs.map((tab, index) => {
+            return (
+              <Tabbar.Item key={index} icon={tab.icon}>
+                {tab.title}
+              </Tabbar.Item>
+            )
+          })}
+        </Tabbar>
+      </div>
+    </div>
   )
 }
 
